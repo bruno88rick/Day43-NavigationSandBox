0cb8444ed73132ebbc0b9d915890e8fb90ce5cd9
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-04-26 11:25:10 -0300

    Initial Commit

---
+++ Day43-NavigationSandBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day43-NavigationSandBox
+//
+//  Created by Bruno Oliveira on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day43-NavigationSandBox/Day43_NavigationSandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day43_NavigationSandBoxApp.swift
+//  Day43-NavigationSandBox
+//
+//  Created by Bruno Oliveira on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Day43_NavigationSandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
