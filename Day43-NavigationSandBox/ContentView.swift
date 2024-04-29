a97a42930079554460b5f39c81845412ceefce13
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-04-29 05:28:45 -0300

    Store NavigationStack path to load when app closes

--- Day43-NavigationSandBox/ContentView.swift
+++ Day43-NavigationSandBox/ContentView.swift
@@ -7,12 +7,57 @@
 
 import SwiftUI
 
+//class to save our navigation data
+
+@Observable
+class PathStore {
+    var path: [Int] { // if you're using NavigationPath() -> var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                //if you're using NavigationPath() -> if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data){
+                path = decoded
+                //if you're using NavigationPath() -> path = NavigationPath(decoded)
+                return
+            }
+        }
+        //Still here? Start with an empty array
+        path = []
+        //if you're using NavigationPath() -> path = NavigationPath()
+    }
+    
+    func save() {
+        
+        //if you're using NavigationPath() ->
+        //guard let representation = path.codable else {return}
+        
+        do {
+            let data = try JSONEncoder().encode(path)
+            //if you're using NavigationPath() -> let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+    
+}
+
 struct ContentView: View {
+
     @State private var path = [Int]()
+    // if you're using NavigationPath() -> @State private var pathStore = PathStore()
     
     var body: some View {
         VStack {
             NavigationStack(path: $path){
+                //if you're using NavigationPath() -> NavigationStack(path: $pathStore.path)
                 DetailView(number: 0, path: $path)
                     .navigationDestination(for: Int.self) { i in
                         DetailView(number: i, path: $path)
